ef92c95298ecb2b76e880bb81fdf4b01ef60f94b
Sadat Mirza <sadatmirza0@gmail.com>
2018-02-13 20:23:48 +0530

    Finally i fetched images!!!

--- app/src/main/java/com/example/android/movietime/MainActivity.java
+++ app/src/main/java/com/example/android/movietime/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.android.movietime;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +15,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 import org.json.JSONArray;
@@ -26,49 +27,87 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private RecyclerView myRecyclerView;
 
     private RecyclerView.Adapter adapter;
 
-    private List<Movies> listOfMovies;
+    private List<Movies> AllMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.RecyclerView_main);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView_main);
+        myRecyclerView.setHasFixedSize(true);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        listOfMovies = new ArrayList<>();
-
-        loadRecyclerViewData();
+        try {
+            loadRecyclerViewData();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadRecyclerViewData(){
-        ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loooading....");
-        progressDialog.show();
+    private void loadRecyclerViewData() throws MalformedURLException {
+
+        new MoviesAsyncTask().execute();
+
     }
 
 /*    http://androidcss.com/android/fetch-json-data-android/*/
 
 
-    private static class MoviesAsyncTask extends AsyncTask<Void, Void, ArrayList<String>> {
-
-        URL urlPopular = new URL("https://api.themoviedb.org/3/movie/popular?api_key=e6119fc0e6963d6ee2300a97c6d1cf22");
+    private class MoviesAsyncTask extends AsyncTask<Void, Void, String> {
 
         //URL urlTopRated = new URL("https://api.themoviedb.org/3/movie/top_rated?api_key=e6119fc0e6963d6ee2300a97c6d1cf22");
 
-        String response;
+        Context context;
 
-        ArrayList<String> output = new ArrayList<>();
-
-        ArrayList<String> result = new ArrayList<>();
 
         private MoviesAsyncTask() throws MalformedURLException {
         }
 
+        private List<Movies> getMovieData(String input)
+                throws JSONException {
+            final String MDB_RESULT = "results";
+            final String MDB_TITLE = "title";
+            final String MDB_POSTER = "poster_path";
+
+            List<Movies> listOfMovies = new ArrayList<>();
+
+            JSONObject moviejson = new JSONObject(input);
+            JSONArray movieArray = moviejson.getJSONArray(MDB_RESULT);
+            String baseURL = "http://image.tmdb.org/t/p/w500/";
+            for (int i = 0; i < movieArray.length(); i++) {
+
+                JSONObject currentMovie = movieArray.getJSONObject(i);
+
+                String movietitle = currentMovie.getString(MDB_TITLE);
+                String moviePosterendURL = currentMovie.getString(MDB_POSTER);
+                String moviePosterURL = baseURL + moviePosterendURL;
+                Movies items = new Movies(movietitle, moviePosterURL);
+                listOfMovies.add(items);
+            }
+
+            return listOfMovies;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            AllMovies = new ArrayList<>();
+            try {
+                AllMovies = getMovieData(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            adapter = new MoviesAdapter(AllMovies,getApplicationContext());
+
+            myRecyclerView.setAdapter(adapter);
+
+            super.onPostExecute(s);
+        }
 
         public String getResponseFromHttpUrl(URL url) throws IOException {
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -89,58 +128,26 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        private ArrayList getMovieData(String input)
-                throws JSONException {
-            final String MDB_RESULT = "results";
-            final String MDB_TITLE = "title";
-            final String MDB_POSTER = "poster_path";
-            String[] movieDetails = new String[20];
-
-            JSONObject moviejson = new JSONObject(input);
-            JSONArray movieArray = moviejson.getJSONArray(MDB_RESULT);
-            String baseURL = "http://image.tmdb.org/t/p/w500/";
-            for (int i = 0; i < 20; i++) {
-                JSONObject currentMovie = movieArray.getJSONObject(i);
-                String movietitle = currentMovie.getString(MDB_TITLE);
-                String moviePosterendURL = currentMovie.getString(MDB_POSTER);
-                String moviePosterURL = baseURL + moviePosterendURL;
-                output.add(moviePosterURL);
-            }
-            return output;
-        }
+        String jsonResponse;
 
         @Override
-        protected ArrayList<String> doInBackground(Void... voids) {
+        protected String doInBackground(Void... voids) {
+
+            URL urlPopular = null;
+            try {
+                urlPopular = new URL("https://api.themoviedb.org/3/movie/popular?api_key=e6119fc0e6963d6ee2300a97c6d1cf22");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
 
             try {
-                response = getResponseFromHttpUrl(urlPopular);
+                jsonResponse = getResponseFromHttpUrl(urlPopular);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-            try {
-                output = getMovieData(response);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            Log.v("Do in background", String.valueOf(output));
-            for (int i = 0 ; i<output.size();i++){
-                result.add(String.valueOf(output));
-            }
-            return result;
+            return jsonResponse;
         }
 
-        /*@Override
-        protected void onPostExecute(Array input) {
-
-            Log.v("Onpostexecute", String.valueOf(output));
-                for (int i = 0 ; i<output.size();i++){
-                    result.add(String.valueOf(output));
-                }
-                return result;
-        }*/
     }
 }
 
